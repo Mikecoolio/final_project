772f5dd2b7b8ad60325b65070f57b8acf24ec474
mikecoolio <mikepoon604@gmail.com>
2022-09-05 20:02:46 -0700

    error message shows up inside graphql now and not just console

--- lib/final_project_web/schema/resolvers/user_resolver.ex
+++ lib/final_project_web/schema/resolvers/user_resolver.ex
@@ -1,14 +1,16 @@
 defmodule FinalProjectWeb.Schema.Resolvers.UserResolver do
   alias FinalProject.Auth
   alias FinalProjectWeb.Utils
+  alias FinalProjectWeb.Constants
+
   def register_user(_, %{input: input}, _) do
     case Auth.create_user(input) do
       {:ok, _} -> {:ok, true}
-      {:error, %Ecto.Changeset{} = changeset} ->
+      {:errors, %Ecto.Changeset{} = changeset} ->
         errors = Utils.format_changeset_errors(changeset)
         {:errors, errors}
 
-      {_, _} -> {:error, "Internal Server Error"} # fallthough case
-    end
+      {_, _} -> {:error, Constants.internal_server_error()}
   end
+end
 end
